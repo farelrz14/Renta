d64ca1bbd2519f689f2a7af77be444ed06c0f386
farelrz14 <67718586+farelrz14@users.noreply.github.com>
2021-10-06 23:35:05 -0600

    creando clase productos

---
+++ BL.Rentas/ProductosBL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+    public class ProductosBL
+    {
+        public ProductosBL()
+        {
+
+        }
+    }
+
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+        public bool Activo { get; set; }
+    }
+}
+
