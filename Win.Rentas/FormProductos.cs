9167234f4cbd5ac8ab69e154a1c47be11e887732
farelrz14 <67718586+farelrz14@users.noreply.github.com>
2021-10-07 01:56:40 -0600

    agregando lista productos

--- Win.Rentas/FormProductos.cs
+++ Win.Rentas/FormProductos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BL.Rentas;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,20 @@ using System.Windows.Forms;
 
 namespace Win.Rentas
 {
+
     public partial class FormProductos : Form
     {
+         
+         ProductosBL _productos;
+
         public FormProductos()
         {
             InitializeComponent();
+
+        _productos = new ProductosBL();
+            listaProductosBindingSource.DataSource = _productos.ObtenerProductos();
+ 
+         
         }
     }
 }
